b5eddf04d5bb5ce1b7abb0ab578d0ceb54439771
Alan Ibañez <alanleonelib@gmail.com>
2025-11-15 11:24:18 -0300

    Footer

--- js/footer.js
+++ js/footer.js
@@ -20,18 +20,18 @@ export class Footer {
         <div class="footer-col">
           <h3>Menu Inferior</h3>
           <ul>
-            <li><a href="/TPIntegradorPW1/index.html">Inicio</a></li>
-            <li><a href="/TPIntegradorPW1/html/cursos.html">Cursos</a></li>
-            <li><a href="/TPIntegradorPW1/html/contacto.html">Contacto</a></li>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="html/cursos.html">Cursos</a></li>
+            <li><a href="html/contacto.html">Contacto</a></li>
           </ul>
         </div>
 
         <div class="footer-col">
           <h3>Redes Sociales</h3>
           <div class="redes">
-            <a href="https://www.facebook.com/" target="_blank"><img src="/TPIntegradorPW1/Imagenes/icono-redes/facebook.png"></a>
-            <a href="https://www.instagram.com/" target="_blank"><img src="/TPIntegradorPW1/Imagenes/icono-redes/instagram.png"></a>
-            <a href="https://www.youtube.com/" target="_blank"><img src="/TPIntegradorPW1/Imagenes/icono-redes/youtube.png"></a>
+            <a href="https://www.facebook.com/" target="_blank"><img src="Imagenes/icono-redes/facebook.png"></a>
+            <a href="https://www.instagram.com/" target="_blank"><img src="Imagenes/icono-redes/instagram.png"></a>
+            <a href="https://www.youtube.com/" target="_blank"><img src="Imagenes/icono-redes/youtube.png"></a>
           </div>
         </div>
       </div>
